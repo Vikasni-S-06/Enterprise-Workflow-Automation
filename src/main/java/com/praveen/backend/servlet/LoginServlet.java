586747604e0f4fafbb9723a9712871bb4e594a00
Vikasni-S-06 <vikasnisaravanakumar@gmail.com>
2026-07-18 23:58:54 +0530

    Completed backend foundation with UserDAO and authentication services

--- src/main/java/com/praveen/backend/servlet/LoginServlet.java
+++ src/main/java/com/praveen/backend/servlet/LoginServlet.java
@@ -1,5 +1,49 @@
 package com.praveen.backend.servlet;
 
-public class LoginServlet {
-    
+import com.praveen.backend.model.User;
+import com.praveen.backend.service.LoginService;
+import com.praveen.backend.service.LoginServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private final LoginService loginService = new LoginServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = loginService.login(email, password);
+
+        response.setContentType("text/plain");
+
+        if (user != null) {
+
+            HttpSession session = request.getSession(true);
+
+            session.setAttribute("loggedInUser", user);
+
+            session.setAttribute("role",
+                    user.getRole().getRoleName());
+
+            response.getWriter().println("LOGIN SUCCESS");
+
+        } else {
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            response.getWriter().println("LOGIN FAILED");
+
+        }
+    }
 }
